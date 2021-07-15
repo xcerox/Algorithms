ae31fb4815beb6135833645121bf990ca657dd45
xcerox <ing.jairoreyes@gmail.com>
2021-07-15 11:40:38 -0400

    completing palindrome algorithm

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,35 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    return isPalindromeUsingEvery(str);
+}
+
+function isPalindromeUsingreverseString(str){
+    return str == str.split("").reverse().join("");
+}
+
+function isPalindromeCheckingHalf(str) {
+    const count = str.length - 1;
+    let isPalindrome = true;
+
+    if (count > 1) {
+        for (index = 0; index< (count + 1) / 2; ++index) {
+            if (str[index] !== str[count - index]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+    }
+
+    return isPalindrome
+}
+
+
+function isPalindromeUsingEvery(str) {
+    return str.split("").every((char, index) => {
+        return char == str[str.length - (index + 1)];
+    });
+}
 
 module.exports = palindrome;
