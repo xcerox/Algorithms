a7396d6c818f82f64fe10f31d3457625988eb013
xcerox <ing.jairoreyes@gmail.com>
2021-07-16 15:11:23 -0400

    feature: completing maxChar Algorithm

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charsCounted = {};
+    let maxChar = "", maxCount = 0;
+
+    for (const char of str) {
+        charsCounted[char] = charsCounted[char] + 1 || 1;
+    }
+
+    for (const key in charsCounted) {
+        if (charsCounted[key] > maxCount) {
+            maxChar = key;
+            maxCount = charsCounted[key];
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
