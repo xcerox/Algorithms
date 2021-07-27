2af48d1f23304cb5494120b169ce7008dd03cbeb
xcerox <ing.jairoreyes@gmail.com>
2021-07-26 21:22:58 -0400

    anagrams algorithms completed

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    mapA = cleanAndSortString(stringA);
+    mapB = cleanAndSortString(stringB);
+
+    return mapA == mapB;
+}
+
+function areAnagramsManual(stringA, stringB) {
+    const mapA = buildCharsMap(stringA);
+    const mapB = buildCharsMap(stringB);
+
+    let isAnagram = Object.keys(mapA).length == Object.keys(mapB).length;
+
+    if (isAnagram) {
+        for (const prop in mapA) {
+            if (mapA[prop] != [mapB[prop]]) {
+                isAnagram = false;
+                break;
+            }
+        }
+    }
+
+    return isAnagram;
+}
+
+
+function buildCharsMap(str){
+    const map = {};
+
+    buildArray(str).forEach(item => map[item] = map[item] + 1 || 1 );
+
+    return map;
+}
+
+function buildArray(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("");
+}
+
+function cleanAndSortString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join(); 
+}
 
 module.exports = anagrams;
