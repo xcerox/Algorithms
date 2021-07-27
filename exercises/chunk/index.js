92e3c74622d600b92466f70530386316865eb1bd
xcerox <ing.jairoreyes@gmail.com>
2021-07-26 20:48:24 -0400

    chunk algoritms completed

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    return getChunkWithSlice(array, size)
+}
+
+function getchunk(array, size) {
+    const chunked = [];
+
+    for (const element of array) {
+        const last = chunked[chunked.length - 1];
+        console.log(chunked.length - 1);
+
+        if (!last || last.length == size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+
+    return chunked;
+}
+
+
+function getChunkWithSlice(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index+size));
+        index += size;
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
