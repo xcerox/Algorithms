742c4873126165b6e4dd88115666c0f3fe657065
Jairo Reyes <ing.jairoreyes@gmail.com>
2021-08-29 12:25:23 -0400

    complement algorithms steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,33 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    printStepsUsingFor(n);
+}
+
+function printStepsUsingFor(n) {
+    let stair;
+    for (let row = 0; row < n; row++) {
+        stair = '';
+
+        for (let column = 0; column < n; column++) {
+            stair += column <= row ? '#': ' ';
+        }
+
+        console.log(stair);
+    }
+}
+
+function printStepsUsingRecursion(n, row = 0, stair = '') {
+    if (n != row) {
+        if (n == stair.length){
+            console.log(stair);
+            printStepsUsingRecursion(n, ++row, '');
+        } else {
+            stair += stair.length <= row ? '#': ' ';
+            printStepsUsingRecursion(n, row, stair);
+        }
+    }
+}
 
 module.exports = steps;
