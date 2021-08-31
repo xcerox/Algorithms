b55f9312dbb40892ce4880693891b256b63fa3cd
Jairo Reyes <ing.jairoreyes@gmail.com>
2021-08-31 16:00:27 -0400

    completing algoritms pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    printPyramidUsingRecursion(n);
+}
+
+function printPyramidUsingRecursion(n, row = 0, level = '', columns = n*2-1, midPoint = Math.floor(columns/2)) {
+    if (n != row) {
+        if (columns == level.length){
+            console.log(level);
+            printPyramidUsingRecursion(n, ++row, '', columns, midPoint);
+        } else {
+            level += midPoint - row <= level.length && midPoint + row >=  level.length ? '#': ' ';
+            printPyramidUsingRecursion(n, row, level, columns, midPoint);
+        }
+    }
+}
+
+function printPyramidUsingFor(n) {
+    const columns = n*2-1;
+    const midPoint = Math.floor((columns/2))
+    let level;
+    for (let row = 0; row < n; row++) {
+        level = '';
+        for (let column = 0; column < columns; column++) {
+            level += midPoint - row <= column && midPoint + row >= column ? '#': ' ';
+        }
+
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
