4f81c426edff7e36e33782cd57ffb94c5ffd8982
Jairo Reyes <ing.jairoreyes@gmail.com>
2021-09-01 16:35:28 -0400

    completing vowels algorithms

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+   return countVowelsUsinREGEX(str);
+}
+
+function countVowelsUsinREGEX(str){
+    return (str.match(/[aeiou]/gi) || []).length;
+}
+
+function countVowelsUsinArray(str){
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return str.toLowerCase().split('').filter(char => vowels.includes(char)).length;
+}
 
 module.exports = vowels;
